35542203dab9d70755dfb2a4400dcaa18e8a1bed
Jouni Miikki <mail@jounimiikki.com>
2019-03-04 16:14:22 +0200

    String exercises 1 started and added

---
+++ python_string_exercises_1.py
@@ -0,0 +1,11 @@
+# Exercise 1, from 'bark' create: 'b','a','r','k' from 'park' create: 'p','a','r','k'
+var = 'bark'
+print(list(var))
+var = 'park'
+print(list(var))
+
+# Exercise 2, from 'pin' create: 'pi','in'
+var = 'pin'
+print(var[0:2])
+print(var[1:3])
+
