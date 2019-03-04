baca17cdc2e458f066eec5d1eeecaaa2e7deb784
Jouni Miikki <mail@jounimiikki.com>
2019-03-04 15:22:32 +0200

    Did 1 to 3 exercises in python_expressions_exercises

---
+++ python_expressions_exercises.py
@@ -0,0 +1,13 @@
+# Python Expressions Exercises (these should be practised on Python Console, but this time I'll write these into file)
+
+# Exercise 1, There are 5280 feet in a mile. Write a Python statement that calculates and prints the number of feet in 23 miles.
+var = 5280 * 23
+print(var)
+
+# Exercise 2, Write a Python statement that calculates and prints the number of seconds in 10 hours, 35 minutes and 20 seconds
+var = ((10*60*60)+(35*60)+24)
+print(var)
+
+# Exercise 3, The perimeter of a rectangle is 2w + 2h, where w and h are the lengths of its sides. Write a Python statement that calculates and prints the length in centimeters of the perimter of a rectangle with sides of length 10 and 15 centimeters.
+var = (2*10)+(2*15)
+print(var)
